fc4988e61d93b2977d40a6ca787e0be68584fa70
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-28 22:57:02 -0700

    Create data directory when app boots up

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flaskr.error import bad_request
 from flaskr import root
+from pathlib import Path
 
 
 def create_app():
@@ -15,6 +16,13 @@ def create_app():
     # We always require a config file to run.
     if not app.config.from_pyfile('config.py'):
         raise FileNotFoundError()
+
+    if 'DATA_DIRECTORY_PATH' not in app.config:
+        raise ValueError('Data directory path missing from the config')
+
+    # Creates the directory that will house the transcripts and LLM indices for each transcript.
+    data_dir = Path(app.config['DATA_DIRECTORY_PATH'])
+    data_dir.mkdir(parents=True, exist_ok=True)
 
     # To support cross-origin requests. This will handle the headers required.
     CORS(app)
