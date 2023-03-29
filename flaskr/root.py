36c8b0a58f7b9b5194958a55bea13e712266d808
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-28 21:30:13 -0700

    Add stub for create-bot API

--- flaskr/root.py
+++ flaskr/root.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 
 bp = Blueprint('root', __name__, url_prefix='/')
 
@@ -7,3 +7,17 @@ bp = Blueprint('root', __name__, url_prefix='/')
 def root():
     """Returns a sanity check about the server running."""
     return jsonify({'output': 'Welcome to YT transcript bot server'})
+
+
+@bp.route('/create-bot', methods=['POST'])
+def create_bot():
+    """Creates a bot for a video."""
+    request_json = request.get_json()
+    if not request_json:
+        abort(400, {'error': 'no JSON data in the request'})
+
+    video_id = request_json.get('video_id')
+    if not video_id:
+        abort(400, {'error': 'video id not provided'})
+
+    return jsonify()
